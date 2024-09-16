ac6af8e79b10494358109b36c1a10b3befb49731
Zareen100 <monzu001976@gmail.com>
2024-09-16 21:51:56 +0600

    Asymptotic Notation

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-def fun1(n):
-    return n*(n+1)/2
-print (fun1(4))
+def printnumber(n):
+    iteration=0
+    print ("total number entered by user",n)
+    iteration+=1
+    print ("total iteration done by computer",iteration)
+    
+printnumber(10)
+printnumber(200)
+
+#actiity 2
+def Ontime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("when n is",n,"iteration=", iteration)
+Ontime(50)
+Ontime(60)
+Ontime(70)
+#activity 3
+def Test(n):
+    iteration=0
+    for i in range(0,n):
+        for j in range(0,n):
+            print("*",end="")
+            iteration+=1
+        print("")
+    print(n,"iteration n=",iteration)
+Test(5)
+Test(10)
+Test(200)
+
+     
